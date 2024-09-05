c915988469df6a9838ceead09c924523e52fe090
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-09-05 17:46:02 +0300

    Полный сериалайзер для фильмов.

--- film_gallary_API/api/serializers.py
+++ film_gallary_API/api/serializers.py
@@ -1,10 +1,38 @@
 from rest_framework import serializers
 
-from films.models import Film
+from films.models import Genre, Film, Person
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    # name = serializers.CharField(source='genre')
+
+    class Meta:
+        model = Genre
+        fields = ('name', )
+
+
+class PersonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Person
+        fields = ('name', )
 
 
 class FilmSerializer(serializers.ModelSerializer):
+    type = serializers.StringRelatedField(read_only=True)
+    country = serializers.StringRelatedField(read_only=True)
+    genre = GenreSerializer(read_only=True, many=True)
+    person = PersonSerializer(read_only=True, many=True)
 
     class Meta:
         model = Film
-        fields = ('title', 'rating_kp', 'rating_imdb', 'release_year')
+        fields = (
+            'title',
+            'rating_kp',
+            'rating_imdb',
+            'release_year',
+            'type',
+            'country',
+            'genre',
+            'person',
+        )
--- film_gallary_API/films/admin.py
+++ film_gallary_API/films/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Country, Genre, Type, Person, Film, WatchedFilms, PlanFilms
+from .models import Country, Genre, Type, Person, Film, WatchedFilms, PlanFilms, FilmGenre, FilmPerson
 
 
 admin.site.register(Country)
@@ -10,4 +10,5 @@ admin.site.register(Person)
 admin.site.register(Film)
 admin.site.register(WatchedFilms)
 admin.site.register(PlanFilms)
-
+admin.site.register(FilmGenre)
+admin.site.register(FilmPerson)
